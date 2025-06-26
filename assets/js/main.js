3ae713b5beb01f0103ac0594b006c5e39e08f43b
Khalidshv <149312120+Khalidshv@users.noreply.github.com>
2025-06-27 00:13:09 +0400

    show menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,24 @@
 /*=============== SEARCH ===============*/
+const searchButton = document.getElementById( 'search-button'),
+      searchClose = document.getElementById('search-close'),
+      searchContent = document.getElementById('search-content');
 
+/*===== MENU SHOW=====*/
+/* Validate if constant exists */
+
+if(searchButton){
+    searchButton.addEventListener('click', () =>{
+    searchContent.classList.add('show-search')
+})
+}
+
+/*===== MENU HIDDEN =====*/
+
+if(searchClose){
+    searchClose.addEventListener('click', () =>{
+    searchContent.classList.remove('show-search')
+})
+}
 
 /*=============== LOGIN ===============*/
 
